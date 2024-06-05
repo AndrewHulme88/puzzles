b11fa0c3407247f6d3d521dbb3deaf51a5fa0321
andrew hulme <andrew_hulme04@hotmail.com>
2024-06-05 10:11:32 +1000

    Added shape match game page

--- script.js
+++ script.js
@@ -27,3 +27,35 @@ linkBoxes.forEach(linkBox => {
         window.location.href = link.id === 'link-1' ? 'game1.html' : link.id === 'link-2' ? 'game2.html' : 'game3.html';
     });
 });
+
+const containers = document.querySelectorAll('.container');
+
+shapes.forEach(shape => {
+    let initialX = shape.offsetLeft;
+    let initialY = shape.offsetTop;
+
+    shape.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData('text', e.target.classList[1]);
+        e.dataTransfer.effectAllowed = 'move';
+    });
+
+    shape.addEventListener('dragend', (e) => {
+        shape.style.top = `${initialY}px`;
+        shape.style.left = `${initialX}px`;
+    });
+});
+
+containers.forEach(container => {
+    container.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+
+    container.addEventListener('drop', (e) => {
+        const shapeClass = e.dataTransfer.getData('text');
+        const shape = document.querySelector(`.${shapeClass}`);
+
+        if (container.classList.contains(shapeClass)) {
+            shape.remove();
+        }
+    });
+});
